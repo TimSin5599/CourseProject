632adc3cfe3cc159f76d7f395102c313c4e4e290
TimSin5599 <125747529+TimSin5599@users.noreply.github.com>
2024-03-22 08:27:56 +0300

    File for extracting attributes

---
+++ extractingAttributes.py
@@ -0,0 +1,111 @@
+import sys
+import tkinter as tk
+from tkinter import messagebox
+from docx import Document
+import spacy
+import re
+import fitz
+from spacy.matcher import Matcher
+from natasha import Segmenter, NewsEmbedding, NewsMorphTagger, NewsSyntaxParser, Doc, NewsNERTagger
+
+nlp = spacy.load("ru_core_news_lg")
+
+def build_path():
+    length = len(sys.argv)
+    if length == 0:
+        return ""
+
+    path = ""
+    for i in range(1, length - 1):
+        path += sys.argv[i] + " "
+    path += sys.argv[length - 1]
+    return path
+
+
+def open_file(path):
+    text = ""
+    extension = path.split('.')[-1]
+
+    if extension == "txt":
+        with open(path, 'r', encoding='utf-8') as file:
+            text = file.read()
+        return text
+    elif extension == "pdf":
+        with fitz.open(path) as pdf_doc:
+            for page_num in range(pdf_doc.page_count):
+                page = pdf_doc[page_num]
+                text += page.get_text()
+        return text
+    elif extension == "docx":
+        doc = Document(path)
+
+        for paragraph in doc.paragraphs:
+            text += paragraph.text + '\n'
+        return text
+    else:
+        root = tk.Tk()
+        root.withdraw()
+        messagebox.showerror("Ошибка", "Данный формат файлов не поддерживается")
+
+        root.destroy()
+        root.quit()
+        exit(0)
+
+
+def get_phone_number(text):
+    phones = re.findall(r"(?:(?:8|\+7)[\- ]?)?(?:\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}", text)
+    if len(phones) == 0:
+        return ""
+    return phones[0]
+
+
+def get_email(text):
+    emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}", text)
+    if len(emails) == 0:
+        return ""
+    return emails[0]
+
+
+def get_name(text):
+    nlp_text = nlp(text)
+    matcher = Matcher(nlp.vocab)
+    pattern = [{'POS': 'PROPN'}, {'POS': 'PROPN'}, {'POS': 'PROPN', 'OP': '?'}]
+    matcher.add('NAME', [pattern])
+    matches = matcher(nlp_text)
+
+    span = None
+    for i in range(len(matches)):
+        match, start, end = matches[i]
+        if span is None:
+            span = nlp_text[start:end]
+        elif matches[i][0] == matches[i-1][0]:
+            span = nlp_text[start:end]
+            break
+        else:
+            break
+    if span is not None:
+        return span.text
+    else:
+        return ""
+
+def get_experience(text):
+    return ""
+
+def get_organization(text):
+    segmenter = Segmenter()
+    emb = NewsEmbedding()
+    morph_tagger = NewsMorphTagger(emb)
+    syntax_parser = NewsSyntaxParser(emb)
+    ner_tagger = NewsNERTagger(emb)
+
+    doc = Doc(text)
+    doc.segment(segmenter)
+    doc.tag_morph(morph_tagger)
+    doc.parse_syntax(syntax_parser)
+    doc.tag_ner(ner_tagger)
+
+    organization = [i.text for i in doc.spans if i.type == "ORG"]
+    return organization
+
+def get_skills(text):
+    return ""
