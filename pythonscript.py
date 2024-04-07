23fe2f37f0988f35c5b06284aaee174a5128c866
TimSin5599 <125747529+TimSin5599@users.noreply.github.com>
2024-04-07 13:00:48 +0300

    File for collection new pdf file

--- pythonscript.py
+++ pythonscript.py
@@ -1,11 +1,12 @@
+import os
+import sys
 from tkinter import filedialog
 import tkinter as tk
 from tkinter import messagebox
 from fpdf import FPDF
-import sys
+import fitz
 
 from extractingAttributes import (
-    build_path,
     open_file,
     get_name,
     get_experience,
@@ -13,13 +14,21 @@ from extractingAttributes import (
     get_phone_number,
     get_attributes_from_model)
 
+path_ = sys.argv[1]
+pathDir = sys.argv[0].split("pythonscript.exe")[0]
+pathFont = pathDir + 'fonts\\AcromRegular.ttf'
+
 def get_text_from_array(array):
     text = ""
+
     for i in range(len(array)):
+        if i == 0:
+            array[i] = array[i].capitalize()
         if i == len(array) - 1:
             text += array[i]
             break
         text += array[i] + ", "
+
     if len(array) == 0:
         text = "-"
     return text
@@ -28,9 +37,22 @@ def createPDF(text, path):
     text.replace('\n','<br />\n')
     pdf = FPDF('P', 'mm', 'A4')
     pdf.add_page()
-    pdf.add_font("AcromRegular", style="", fname=sys.argv[0].split("pythonscript.py")[0] + 'AcromRegular.ttf', uni=True)
+    pdf.add_font("AcromRegular", style="", fname=pathFont, uni=True)
     pdf.set_font('AcromRegular', '', 18)
-    pdf.multi_cell(0,5,'Ключевые данные из резюме:\n\n')
+
+    doc = fitz.open(path_)
+    for i in range(len(doc)):
+        for img in doc.get_page_images(i):
+            xref = img[0]
+            pix = fitz.Pixmap(doc, xref)
+            pix1 = fitz.Pixmap(fitz.csRGB, pix)
+            if (pix1.w >= 200 and pix1.h >= 200):
+                pix1.save(pathDir + "image.png")
+                pdf.image(pathDir + "image.png", w=40, h=40)
+                os.remove(pathDir + "image.png")
+                break
+
+    pdf.multi_cell(0,5,'\nКлючевые данные из резюме:\n\n')
     pdf.set_font('AcromRegular', '', 12)
     pdf.multi_cell(0,5, text)
     pdf.output(path)
@@ -42,30 +64,25 @@ def createPDF(text, path):
     root.quit()
 
 if __name__ == '__main__':
-    path_ = build_path()
     text_ = open_file(path_)
+    # print(text_)
+    dictionary = get_attributes_from_model(text_)
 
-    name, skills, edu, org, languages, self_summary, speciality, faculty, adr, data = get_attributes_from_model(text_)
-    if len(data) != 0:
-        data = [data[0]]
-    if len(name) != 0:
-        person = get_name(text_)
-    else:
-        person = "-"
+    person = get_name(text_)
     text = ""
-    text += person + "\n"
-    text += "Дата рождения: " + get_text_from_array(list(set(data))) + "\n"
+    text += "ФИО: " + person + "\n"
+    text += "Дата рождения: " + get_text_from_array(list(dictionary["DATA"])) + "\n"
     text += "Номер телефона: " + get_phone_number(text_) + "\n"
     text += "Email: " + get_email(text_) + "\n"
-    text += "Адрес: " + get_text_from_array(list(set(adr))) + "\n"
-    text += "Образовательные учреждения: " + "\n" + get_text_from_array(list(set(edu))) + "\n"
-    text += "Факультеты: " + get_text_from_array(list(set(faculty))) + "\n"
-    text += "Специальности: " + get_text_from_array(list(set(speciality))) + "\n"
-    text += "Организации: " + "\n" + get_text_from_array(list(set(org))) + "\n"
+    text += "Адрес: " + get_text_from_array(list(dictionary["ADR"])) + "\n"
+    text += "Образовательные учреждения: " + "\n" + get_text_from_array(list(dictionary["EDU"])) + "\n"
+    text += "Факультеты: " + get_text_from_array(list(dictionary["FACULTY"])) + "\n"
+    text += "Специальности: " + get_text_from_array(list(dictionary["SPECIALTY"])) + "\n"
+    text += "Организации: " + "\n" + get_text_from_array(list(dictionary["ORG"])) + "\n"
     text += "Опыт работы: " + "\n" + get_text_from_array(get_experience(text_)) + "\n"
-    text += "Языки: " + get_text_from_array(list(set(languages))) + "\n"
-    text += "Ключевые навыки: " + "\n" + get_text_from_array(list(set(skills))) + "\n"
-    text += "Личная информация: " + "\n" + get_text_from_array(list(set(self_summary))) + "\n"
+    text += "Языки: " + get_text_from_array(list(dictionary["LANGUAGES"])) + "\n"
+    text += "Ключевые навыки: " + "\n" + get_text_from_array(list(dictionary["SKILLS"])) + "\n"
+    text += "Личная информация: " + "\n" + get_text_from_array(list(dictionary["SELF_SUMMARY"])) + "\n"
 
     root = tk.Tk()
     root.withdraw()
